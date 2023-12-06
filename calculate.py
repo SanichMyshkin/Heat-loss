07e3d0ac2a11b7b89c02d5ce1f76bf15b1cdfe22
Александр Мышкин <sir.myshkin@gmail.com>
2023-12-06 17:59:54 +0300

    Add wals combobox and valid wals

--- calculate.py
+++ calculate.py
@@ -30,21 +30,16 @@ class CalculateTable():
         self.CalculateRoomsComboBox()
         self.MakeCalculateCellReadOnly()
 
-        # Получите номер новой строки
         current_row = self.CalculateTableWidget.rowCount() - 1
-
-        # Получите QComboBox в новой строке для комнаты
+        wall_combo_box = self.CalculateTableWidget.cellWidget(current_row, 4)
+        wall_combo_box.currentIndexChanged.connect(self.Get_wall_coeff)
         room_combo_box1 = self.CalculateTableWidget.cellWidget(current_row, 0)
-
-        # Подключите сигнал currentIndexChanged к функции Get_temp
         room_combo_box1.currentIndexChanged.connect(self.Get_temp)
-
         room_combo_box2 = self.CalculateTableWidget.cellWidget(current_row, 1)
-
-        # Подключите сигнал currentIndexChanged к функции Get_temp
         room_combo_box2.currentIndexChanged.connect(self.Get_temp)
-        # Вызовите Get_temp для обновления значений в новой строке
+
         self.Get_temp()
+        self.Get_wall_coeff()
 
     def RemoveCalculateRow(self):
         self.CalculateTableWidget.removeRow(
@@ -224,6 +219,7 @@ class CalculateTable():
 
         self.list_walls = table_contents
         self.CalculateWallsComboBox()
+        self.Get_wall_coeff()
 
     def CalculateWallsComboBox(self):
         column_number = [4]
@@ -251,6 +247,7 @@ class CalculateTable():
                 index = combo_box.findText(current_text)
                 if index != -1:
                     combo_box.setCurrentIndex(index)
+                combo_box.currentIndexChanged.connect(self.Get_wall_coeff)
 
     def parse_walls(self, data):
         result = []
@@ -267,13 +264,6 @@ class CalculateTable():
             row_data = [self.CalculateTableWidget.cellWidget(
                 row, col).currentText()]
             self.list_walls_position.append(row_data)
-
-    def ShowCalculate(self):
-        if self.valid_rooms() is False:
-            message_box = QMessageBox()
-            message_box.critical(
-                    None, "Ошибка!", f"Помещения устарели!\nПожалуйcта, обновите помещения!")  # noqa E501
-            message_box.setFixedSize(500, 200)
 
     def Get_temp(self):
         column_number = [0, 1]
@@ -321,3 +311,59 @@ class CalculateTable():
                         }
             current_table.append(row_data)
         return self.compare_rooms(current_table, self.list_rooms)
+
+    def Get_wall_coeff(self):
+        col = 4
+        wall_dict = {}
+        for row in range(self.WallsTableWidget.rowCount()):
+            name_wall = self.WallsTableWidget.item(row, 0).text()
+            R = self.WallsTableWidget.item(row, 7).text()
+            wall_dict[name_wall] = R
+
+        for row in range(self.CalculateTableWidget.rowCount()):
+            name_wall = self.CalculateTableWidget.cellWidget(
+                row, col).currentText()
+            R = wall_dict.get(name_wall, 0.0)
+            R_item = QTableWidgetItem()
+            R_item.setData(0, f'{R}')
+            self.CalculateTableWidget.setItem(row, col + 4, R_item)
+            self.CalculateTableWidget.item(
+                row, col + 4).setText(f'{R}')
+
+    def valid_walls(self):
+        # Получить текущие данные о стенах из таблицы
+        current_table = []
+        for row in range(self.WallsTableWidget.rowCount()):
+            current_wall_name_item = self.WallsTableWidget.item(row, 0)
+            current_coefficient_item = self.WallsTableWidget.item(row, 7)
+
+            wall_name = current_wall_name_item.text()
+            coefficient_value = current_coefficient_item.text()
+
+            row_data = {'wall_name': wall_name,
+                        'coefficient_value': coefficient_value}
+            current_table.append(row_data)
+
+        # Сравнить текущие данные о стенах с сохраненными данными
+        return self.compare_walls(current_table, self.list_walls)
+
+    def compare_walls(self, list1, list2):
+        set1 = {(item['wall_name'], item['coefficient_value'])
+                for item in list1}
+        set2 = {(item['wall_name'], item['coefficient_value'])
+                for item in list2}
+
+        # Если множества совпадают, данные считаются актуальными
+        return set1 == set2
+
+    def ShowCalculate(self):
+        if self.valid_rooms() is False:
+            message_box = QMessageBox()
+            message_box.critical(
+                    None, "Ошибка!", f"Помещения устарели!\nПожалуйcта, обновите помещения!")  # noqa E501
+            message_box.setFixedSize(500, 200)
+        if self.valid_walls() is False:
+            message_box = QMessageBox()
+            message_box.critical(
+                    None, "Ошибка!", f"Стены устарели!\nПожалуйcта, обновите стены!")  # noqa E501
+            message_box.setFixedSize(500, 200)
